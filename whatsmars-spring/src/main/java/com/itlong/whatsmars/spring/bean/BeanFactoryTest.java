efaadcc6a9d89107ce03b6aa351d390fe999d2aa
javahongxi <javahongxi@qq.com>
2016-08-23 07:46:16 +0800

    BeanFactoryTest

---
+++ whatsmars-spring/src/main/java/com/itlong/whatsmars/spring/bean/BeanFactoryTest.java
@@ -0,0 +1,24 @@
+package com.itlong.whatsmars.spring.bean;
+
+import com.itlong.whatsmars.spring.Mars;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.core.io.support.ResourcePatternResolver;
+
+/**
+ * Created by shenhongxi on 16/8/23.
+ */
+public class BeanFactoryTest {
+
+    public static void main(String[] args) {
+        ResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+        Resource res = resolver.getResource("classpath:spring-test.xml");
+        BeanFactory bf = new XmlBeanFactory(res);
+        System.out.println("init BeanFactory");
+
+        Mars mars = bf.getBean("mars", Mars.class);
+        System.out.println(mars.getCnName() + ":" + mars.getAge());
+    }
+}
