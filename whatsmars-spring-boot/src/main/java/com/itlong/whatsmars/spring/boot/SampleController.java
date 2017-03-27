71f9dcf0018644dfc1a12227b35fac99105a5b70
javahongxi <javahongxi@qq.com>
2017-03-27 17:18:17 +0800

    spring boot example

---
+++ whatsmars-spring-boot/src/main/java/com/itlong/whatsmars/spring/boot/SampleController.java
@@ -0,0 +1,25 @@
+package com.itlong.whatsmars.spring.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by Administrator on 2017/3/27.
+ */
+@Controller
+@EnableAutoConfiguration
+public class SampleController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(SampleController.class, args);
+    }
+}
