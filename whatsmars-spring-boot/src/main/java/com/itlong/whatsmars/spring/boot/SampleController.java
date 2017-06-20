3e0f0f86357166361b73c34840580d60e99cc2ee
shenhongxi <shenhongxi@360.cn>
2017-06-20 19:35:27 +0800

    springboot

--- whatsmars-spring-boot/src/main/java/com/itlong/whatsmars/spring/boot/SampleController.java
+++ whatsmars-spring-boot/src/main/java/com/itlong/whatsmars/spring/boot/SampleController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
- * Created by Administrator on 2017/3/27.
+ * Created by shenhongxi on 2017/3/27.
  */
 @Controller
 @EnableAutoConfiguration
