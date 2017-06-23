ed9905da381e99a5d020aa8c736305dba034b0f1
shenhongxi <shenhongxi@360.cn>
2017-06-23 14:28:46 +0800

    EnableAutoConfiguration

--- whatsmars-spring-boot/src/main/java/com/itlong/whatsmars/spring/boot/Application.java
+++ whatsmars-spring-boot/src/main/java/com/itlong/whatsmars/spring/boot/Application.java
@@ -1,10 +1,13 @@
 package com.itlong.whatsmars.spring.boot;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 @EnableConfigurationProperties({UserConfig.class})
 public class Application {
 
--- whatsmars-spring-boot/src/main/java/com/itlong/whatsmars/spring/boot/UserController.java
+++ whatsmars-spring-boot/src/main/java/com/itlong/whatsmars/spring/boot/UserController.java
@@ -1,7 +1,6 @@
 package com.itlong.whatsmars.spring.boot;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,7 +11,6 @@ import java.util.Map;
  * Created by shenhongxi on 2017/3/27.
  */
 @Controller
-@EnableAutoConfiguration
 public class UserController {
 
     @Autowired
