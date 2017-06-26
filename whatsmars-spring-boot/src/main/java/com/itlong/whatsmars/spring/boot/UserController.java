453c3929139d0101af411cf4c4d46418e3d0f36e
shenhongxi <shenhongxi@360.cn>
2017-06-26 15:22:22 +0800

    i18n

--- whatsmars-spring-boot/src/main/java/com/itlong/whatsmars/spring/boot/UserController.java
+++ whatsmars-spring-boot/src/main/java/com/itlong/whatsmars/spring/boot/UserController.java
@@ -1,10 +1,16 @@
 package com.itlong.whatsmars.spring.boot;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.FixedLocaleResolver;
 
+import java.util.Locale;
 import java.util.Map;
 
 /**
@@ -16,9 +22,26 @@ public class UserController {
     @Autowired
     private UserConfig userConfig;
 
+    @Autowired
+    private MessageSource messageSource;
+
+    /**
+     * 设置区域解析器 (default is AcceptHeaderLocaleResolver)
+     */
+    @Bean
+    public LocaleResolver localeResolver() {
+        FixedLocaleResolver slr = new FixedLocaleResolver ();
+        slr.setDefaultLocale(Locale.US);
+        return slr;
+
+    }
+
     @RequestMapping("/")
     public String home(Map<String,Object> map) {
         map.put("hello", "Hi, boy!");
+        Locale locale = LocaleContextHolder.getLocale();
+        String country = messageSource.getMessage("country", null, locale);
+        map.put("country", country);
         return "index";
     }
 
