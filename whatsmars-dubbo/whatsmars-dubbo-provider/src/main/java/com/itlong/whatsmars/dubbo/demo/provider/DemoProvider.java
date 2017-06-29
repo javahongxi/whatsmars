8402daa2b2e6fc0751d43f8070ec85163381dd95
shenhongxi <shenhongxi@360.cn>
2017-06-29 14:08:22 +0800

    properties配置

--- whatsmars-dubbo/whatsmars-dubbo-provider/src/main/java/com/itlong/whatsmars/dubbo/demo/provider/DemoProvider.java
+++ whatsmars-dubbo/whatsmars-dubbo-provider/src/main/java/com/itlong/whatsmars/dubbo/demo/provider/DemoProvider.java
@@ -3,19 +3,13 @@
  */
 package com.itlong.whatsmars.dubbo.demo.provider;
 
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-
 import java.io.IOException;
 
 public class DemoProvider {
 
 	public static void main(String[] args) throws IOException {
 
-		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[] {"application.xml"});
-		context.start();
-
-		System.out.println("service started");
-		System.in.read();
+		com.alibaba.dubbo.container.Main.main(args);
 
 	}
 
