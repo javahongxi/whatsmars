bf8076184e0c88a0ce168afaace4527d54e23ddd
shenhongxi <shenhongxi@360.cn>
2017-06-29 14:50:19 +0800

    springboot-dubbo

--- whatsmars-spring-boot/src/main/java/com/itlong/whatsmars/spring/boot/App.java
+++ whatsmars-spring-boot/src/main/java/com/itlong/whatsmars/spring/boot/App.java
@@ -5,11 +5,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
 // 加上exclude=DataSource..后，不会连接数据库，同时@Mapper也不会起作用
 @EnableAutoConfiguration//(exclude={DataSourceAutoConfiguration.class})
 @EnableConfigurationProperties({UserConfig.class})
+@ImportResource(locations={"classpath:spring/dubbo-demo-consumer.xml"})
 public class App {
 
     public static void main(String[] args) {
