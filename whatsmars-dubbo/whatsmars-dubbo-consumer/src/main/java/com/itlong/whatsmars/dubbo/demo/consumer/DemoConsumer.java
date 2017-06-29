c1af4a0e859298d717405b31e2333fd94fae29b5
shenhongxi <shenhongxi@360.cn>
2017-06-29 14:13:41 +0800

    properties配置

--- whatsmars-dubbo/whatsmars-dubbo-consumer/src/main/java/com/itlong/whatsmars/dubbo/demo/consumer/DemoConsumer.java
+++ whatsmars-dubbo/whatsmars-dubbo-consumer/src/main/java/com/itlong/whatsmars/dubbo/demo/consumer/DemoConsumer.java
@@ -3,14 +3,12 @@
  */
 package com.itlong.whatsmars.dubbo.demo.consumer;
 
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-
 public class DemoConsumer {
 	
 	public static void main(String[] args) {
 
-		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[] {"application.xml"});
-		context.start();
+		com.alibaba.dubbo.container.Main.main(args);
+
 	}
 
 }
