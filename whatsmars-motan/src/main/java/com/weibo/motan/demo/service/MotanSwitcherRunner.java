9ab4a62be2cb71ac417ad497a2934aba59753a61
shenhongxi <shenhongxi@360.cn>
2017-06-30 17:40:24 +0800

    MotanSwitcherRunner

---
+++ whatsmars-motan/src/main/java/com/weibo/motan/demo/service/MotanSwitcherRunner.java
@@ -0,0 +1,21 @@
+package com.weibo.motan.demo.service;
+
+import com.weibo.api.motan.common.MotanConstants;
+import com.weibo.api.motan.util.MotanSwitcherUtil;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by shenhongxi on 2017/6/30.
+ */
+@Component
+@Order(value = 1)
+public class MotanSwitcherRunner implements CommandLineRunner {
+    @Override
+    public void run(String... args) throws Exception {
+        // 在使用注册中心时要主动调用下面代码
+        MotanSwitcherUtil.setSwitcherValue(MotanConstants.REGISTRY_HEARTBEAT_SWITCHER, true);
+        System.out.println("server start...");
+    }
+}
