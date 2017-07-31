e3455ad6f15fdfd617a72d7d786efdec83d6eded
shenhongxi <shenhongxi@360.cn>
2017-07-31 16:48:55 +0800

    LocaleUtils

---
+++ whatsmars-spring-boot/src/main/java/com/itlong/whatsmars/spring/boot/common/LocaleUtils.java
@@ -0,0 +1,13 @@
+package com.itlong.whatsmars.spring.boot.common;
+
+import org.springframework.context.i18n.LocaleContextHolder;
+
+/**
+ * Created by shenhongxi on 2017/7/31.
+ */
+public class LocaleUtils {
+
+    public static boolean isEnLocale() {
+        return LocaleContextHolder.getLocale().getLanguage().equals("en");
+    }
+}
