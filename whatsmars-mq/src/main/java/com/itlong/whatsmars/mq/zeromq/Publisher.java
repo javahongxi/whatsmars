9d884da509b86fe40aad2f987d5fff5156ebe2d1
shenhongxi <shenhongxi@360.cn>
2017-09-11 16:19:44 +0800

    zeromq

---
+++ whatsmars-mq/src/main/java/com/itlong/whatsmars/mq/zeromq/Publisher.java
@@ -0,0 +1,21 @@
+package com.itlong.whatsmars.mq.zeromq;
+
+import org.zeromq.ZMQ;
+
+public class Publisher {
+
+    public static void main(String args[]) {
+        ZMQ.Context context = ZMQ.context(1); // 创建包含一个I/O线程的context
+        ZMQ.Socket publisher = context.socket(ZMQ.PUB);
+        publisher.bind("tcp://*:5555");
+ 
+        while (!Thread.currentThread ().isInterrupted()) {
+            String message = "toutiao hello";
+            publisher.send(message.getBytes());
+            System.out.println("sent : " + message);
+        }  
+ 
+        publisher.close();  
+        context.term();  
+    }  
+}
