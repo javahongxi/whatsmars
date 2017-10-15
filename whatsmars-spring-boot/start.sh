61f529ffb295a33fbdcc5519972069b20a6cced9
javahongxi <javahongxi@qq.com>
2017-10-16 06:43:19 +0800

    start.sh

--- whatsmars-spring-boot/start.sh
+++ whatsmars-spring-boot/start.sh
@@ -1,6 +1,8 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 app_name="whatsmars-spring-boot"
-app_profiles="test"
+app_profiles="dev"
 
-nohup java -Xmx1024m -Xms1024m -XX:-PrintGC -XX:+PrintGCDetails -XX:+PrintGCTimeStamps -XX:+PrintHeapAtGC -XX:+PrintTenuringDistribution -XX:+HeapDumpOnOutOfMemoryError -Xloggc:./gc.log -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 -jar ${app_name}.jar --spring.profiles.active=${app_profiles} >./console.log 2>&1 &
+mvn clean package -Dmaven.test.skip=true
+
+nohup java -Xmx1024m -Xms1024m -XX:-PrintGC -XX:+PrintGCDetails -XX:+PrintGCTimeStamps -XX:+PrintHeapAtGC -XX:+PrintTenuringDistribution -XX:+HeapDumpOnOutOfMemoryError -Xloggc:./gc.log -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 -jar target/${app_name}.jar --spring.profiles.active=${app_profiles} >./console.log 2>&1 &
