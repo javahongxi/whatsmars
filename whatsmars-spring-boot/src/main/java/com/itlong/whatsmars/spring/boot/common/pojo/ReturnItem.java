c2f8209c10987a4b2b95706e6e739bef9ee9f674
javahongxi <javahongxi@qq.com>
2017-11-16 18:52:02 +0800

    异常信息拦截

---
+++ whatsmars-spring-boot/src/main/java/com/itlong/whatsmars/spring/boot/common/pojo/ReturnItem.java
@@ -0,0 +1,54 @@
+package com.itlong.whatsmars.spring.boot.common.pojo;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+public class ReturnItem<T> {
+
+	/* 请求状态 */
+	private int status;
+	/* 查询结果 */
+	private T item;
+
+	@JsonIgnore
+	private String[] ignoreFields = new String[]{};
+
+	public int getStatus() {
+		return status;
+	}
+
+	public void setStatus(int status) {
+		this.status = status;
+	}
+
+	public T getItem() {
+		return item;
+	}
+
+	public void setItem(T item) {
+		this.item = item;
+	}
+
+    public String[] getIgnoreFields() {
+        return ignoreFields;
+    }
+
+    public void setIgnoreFields(String[] ignoreFields) {
+        this.ignoreFields = ignoreFields;
+    }
+
+    public ReturnItem() {
+		super();
+	}
+
+	public ReturnItem(int status, T item) {
+		super();
+		this.status = status;
+		this.item = item;
+	}
+
+    public ReturnItem(int status, T item, String[] ignoreFields) {
+        this.status = status;
+        this.item = item;
+        this.ignoreFields = ignoreFields;
+    }
+}
---
+++ whatsmars-spring-boot/src/main/java/com/itlong/whatsmars/spring/boot/controller/NewController.java
@@ -0,0 +1,30 @@
+package com.itlong.whatsmars.spring.boot.controller;
+
+import com.itlong.whatsmars.spring.boot.exception.BusinessException;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by shenhongxi-os on 2017/11/16.
+ */
+@RestController
+@RequestMapping("/new")
+public class NewController {
+
+    @RequestMapping("/t")
+    public Map<String, Object> t() {
+        Map<String, Object> m = new HashMap<String, Object>();
+        m.put("domain", "toutiao.im");
+        return m;
+    }
+
+    @RequestMapping("/e")
+    public String e(@RequestParam(required = false) String error) {
+        if (error != null) throw BusinessException.build(error);
+        return "OK";
+    }
+}
