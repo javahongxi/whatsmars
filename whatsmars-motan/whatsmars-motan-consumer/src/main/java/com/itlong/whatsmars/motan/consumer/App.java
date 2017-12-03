dcf20de2765d050561e5c9e0ce8a95bc90f1db6d
javahongxi <javahongxi@qq.com>
2017-12-03 22:20:46 +0800

    去掉多余注解

--- whatsmars-motan/whatsmars-motan-consumer/src/main/java/com/itlong/whatsmars/motan/consumer/App.java
+++ whatsmars-motan/whatsmars-motan-consumer/src/main/java/com/itlong/whatsmars/motan/consumer/App.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
-@EnableAutoConfiguration
 // 注意：依赖包里满足条件的配置也会加载进来，这里以demo开头确保只加载本module下的配置
 @ImportResource(locations={"classpath*:spring/demo*.xml"})
 public class App {
--- whatsmars-motan/whatsmars-motan-provider/src/main/java/com/itlong/whatsmars/motan/service/App.java
+++ whatsmars-motan/whatsmars-motan-provider/src/main/java/com/itlong/whatsmars/motan/service/App.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
-@EnableAutoConfiguration
 @ImportResource(locations={"classpath*:spring/*server.xml"})
 public class App {
 
