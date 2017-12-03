a8a3a39df97aecafcea4f07893b9b7f95cdc4ba3
javahongxi <javahongxi@qq.com>
2017-12-04 03:25:24 +0800

    spring-boot-starter-dubbo demo

---
+++ whatsmars-dubbo/whatsmars-dubbo-provider-boot/src/main/java/com/whatsmars/dubbo/demo/provider/App.java
@@ -0,0 +1,14 @@
+package com.whatsmars.dubbo.demo.provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by javahongxi on 2017/12/4.
+ */
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
---
+++ whatsmars-dubbo/whatsmars-dubbo-provider-boot/src/main/java/com/whatsmars/dubbo/demo/provider/resource/DemoServiceImpl.java
@@ -0,0 +1,21 @@
+/**
+ * Created by shenhongxi on 2017/12/4.
+ */
+package com.whatsmars.dubbo.demo.provider.resource;
+
+import com.alibaba.dubbo.config.annotation.Service;
+import com.alibaba.dubbo.rpc.RpcContext;
+import com.itlong.whatsmars.dubbo.demo.DemoService;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Service(version = "1.0.0")
+public class DemoServiceImpl implements DemoService {
+
+    public String sayHello(String name) {
+        System.out.println("[" + new SimpleDateFormat("HH:mm:ss").format(new Date()) + "] Hello " + name + ", request from consumer: " + RpcContext.getContext().getRemoteAddress());
+        return "Hello " + name + ", response form provider: " + RpcContext.getContext().getLocalAddress();
+    }
+    
+}
