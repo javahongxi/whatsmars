b383dfca66f1c029d9b784d4616b43b41447b4c9
javahongxi <javahongxi@qq.com>
2017-12-04 22:58:17 +0800

    HTTP协议简单解析

--- whatsmars-rpc/src/main/java/com/whatsmars/rpc/protocol/http/HttpRequestDecoder.java
+++ whatsmars-rpc/src/main/java/com/whatsmars/rpc/protocol/http/HttpRequestDecoder.java
@@ -1,6 +1,5 @@
 package com.whatsmars.rpc.protocol.http;
 
-import com.whatsmars.grpc.service.HelloRequest;
 import org.apache.mina.core.buffer.IoBuffer;
 import org.apache.mina.core.session.IoSession;
 import org.apache.mina.filter.codec.CumulativeProtocolDecoder;
@@ -11,7 +10,8 @@ import java.util.Arrays;
 /**
  * Created by shenhongxi on 2017/6/14.
  */
-public class HttpRequestDecoder  extends CumulativeProtocolDecoder {
+public class HttpRequestDecoder extends CumulativeProtocolDecoder {
+
     @Override
     protected boolean doDecode(IoSession ioSession, IoBuffer ioBuffer, ProtocolDecoderOutput out) throws Exception {
         int start = ioBuffer.position();
@@ -21,7 +21,7 @@ public class HttpRequestDecoder  extends CumulativeProtocolDecoder {
             //content = inBuffer.getString(Charset.forName("UTF-8").newDecoder());
             messages = new byte[ioBuffer.limit()];
             ioBuffer.get(messages);
-            content = new String(messages,"UTF-8");
+            content = new String(messages, "UTF-8");
         } catch (Exception e) {
             ioBuffer.position(start);
             return false;
@@ -44,25 +44,24 @@ public class HttpRequestDecoder  extends CumulativeProtocolDecoder {
             reqMsg.addRequestHeader(temps[0].trim().toLowerCase(), temps[1].trim());
         }
 
-        int fLPosition = headerContent.indexOf("\r\n");
-        String url = headerContent.substring(0, fLPosition);
-        String[] urls = url.split(" ");
+        String requestLine = headerContent.substring(0, headerContent.indexOf("\r\n"));
+        String[] lineData = requestLine.split(" ");
 
-        if (urls.length < 3) {
+        if (lineData.length < 3) {
             reqMsg.setErrorCode(400);
             out.write(reqMsg);
             return true;
         }
 
-        if (!"GET".equalsIgnoreCase(urls[0]) && !"POST".equalsIgnoreCase(urls[0])) {
+        if (!"GET".equalsIgnoreCase(lineData[0]) && !"POST".equalsIgnoreCase(lineData[0])) {
             reqMsg.setErrorCode(400);
             out.write(reqMsg);
             return true;
         }
 
-        reqMsg.setRequestProtocol(urls[0]);
-        reqMsg.setRequestUrl(urls[1]);
-        reqMsg.setVersion(urls[2]);
+        reqMsg.setRequestProtocol(lineData[0]);
+        reqMsg.setRequestUrl(lineData[1]);
+        reqMsg.setVersion(lineData[2]);
 
         String bodyLengthStr = reqMsg.getRequestHeader("Content-Length".toLowerCase());
         if (bodyLengthStr == null) {
@@ -88,15 +87,7 @@ public class HttpRequestDecoder  extends CumulativeProtocolDecoder {
             ioBuffer.position(packageLength);
             int l = messages.length;
             byte[] bodyBytes = Arrays.copyOfRange(messages, headerLength, packageLength);
-            String requestName = reqMsg.getRequestHeader("Request-Name".toLowerCase());
-            if (requestName != null && !requestName.equals("")) {
-                if (requestName.equals("hello")) {
-                    HelloRequest helloRequest = HelloRequest.parseFrom(bodyBytes);
-
-                }
-            } else {
-                bodyContent = new String(bodyBytes, "UTF-8");
-            }
+            bodyContent = new String(bodyBytes, "UTF-8");
         } else {
             ioBuffer.position(start);
             return false;
