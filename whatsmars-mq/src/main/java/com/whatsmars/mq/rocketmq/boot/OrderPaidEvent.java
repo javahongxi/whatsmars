35e99382d133ee5ea8c430b37f90160c0d00273b
javahongxi <javahongxi@qq.com>
2017-12-05 21:40:40 +0800

    spring-boot-starter-rocketmq demo

---
+++ whatsmars-mq/src/main/java/com/whatsmars/mq/rocketmq/boot/OrderPaidEvent.java
@@ -0,0 +1,33 @@
+package com.whatsmars.mq.rocketmq.boot;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+public class OrderPaidEvent implements Serializable {
+        private String orderId;
+        
+        private BigDecimal paidMoney;
+
+        public OrderPaidEvent() {}
+
+        public OrderPaidEvent(String orderId, BigDecimal paidMoney) {
+            this.orderId = orderId;
+            this.paidMoney = paidMoney;
+        }
+
+        public String getOrderId() {
+            return orderId;
+        }
+
+        public void setOrderId(String orderId) {
+            this.orderId = orderId;
+        }
+
+        public BigDecimal getPaidMoney() {
+            return paidMoney;
+        }
+
+        public void setPaidMoney(BigDecimal paidMoney) {
+            this.paidMoney = paidMoney;
+        }
+    }
