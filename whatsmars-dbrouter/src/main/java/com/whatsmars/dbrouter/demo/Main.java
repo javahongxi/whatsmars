84e5eddd58b4722daa8dc277992df0bc088b9fee
javahongxi <javahongxi@qq.com>
2017-12-18 04:17:23 +0800

    分库分表demo

--- whatsmars-dbrouter/src/main/java/com/whatsmars/dbrouter/demo/Main.java
+++ whatsmars-dbrouter/src/main/java/com/whatsmars/dbrouter/demo/Main.java
@@ -16,6 +16,12 @@ public class Main {
         AddOrderReq addOrderReq = new AddOrderReq();
         addOrderReq.setUserId("jd123456789");
         addOrderReq.setAmount(new BigDecimal(1000));
-        orderService.addOrder(addOrderReq);
+        try {
+            orderService.addOrder(addOrderReq);
+        } catch (Exception e) {
+            if (e.getMessage().contains("Table 'demo_ds_0.order_0' doesn't exist")) {
+                System.out.println("TEST SUCCESSFUL");
+            }
+        }
     }
 }
