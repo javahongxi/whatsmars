0416876f3cad0ea10b01c9ccb1a02ad2fe8665f9
javahongxi <javahongxi@qq.com>
2017-12-23 04:03:23 +0800

    whatsmars-boot-sample-aop

---
+++ whatsmars-spring-boot-samples/whatsmars-boot-sample-aop/src/main/java/com/whatsmars/boot/sample/aop/SampleAopApplication.java
@@ -0,0 +1,26 @@
+package com.whatsmars.boot.sample.aop;
+
+import com.whatsmars.boot.sample.aop.service.HelloWorldService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleAopApplication implements CommandLineRunner {
+
+	// Simple example shows how an application can spy on itself with AOP
+
+	@Autowired
+    private HelloWorldService helloWorldService;
+
+	@Override
+	public void run(String... args) {
+		System.out.println(this.helloWorldService.getHelloMessage());
+	}
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(SampleAopApplication.class, args);
+	}
+
+}
---
+++ whatsmars-spring-boot-samples/whatsmars-boot-sample-aop/src/main/java/com/whatsmars/boot/sample/aop/monitor/ServiceMonitor.java
@@ -0,0 +1,18 @@
+package com.whatsmars.boot.sample.aop.monitor;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class ServiceMonitor {
+
+	@AfterReturning("execution(* com.whatsmars.boot.sample..*Service.*(..))")
+    public void logServiceAccess(JoinPoint joinPoint) {
+		System.out.println("Completed: " + joinPoint);
+	}
+
+}
