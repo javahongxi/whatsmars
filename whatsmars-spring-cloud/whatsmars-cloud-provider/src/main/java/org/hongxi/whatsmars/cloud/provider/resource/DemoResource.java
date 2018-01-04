c1a47bb7cc6aa264237ec635cf45a37794a9298b
javahongxi <javahongxi@qq.com>
2018-01-05 02:19:45 +0800

    rename package name

---
+++ whatsmars-spring-cloud/whatsmars-cloud-provider/src/main/java/org/hongxi/whatsmars/cloud/provider/resource/DemoResource.java
@@ -0,0 +1,19 @@
+package org.hongxi.whatsmars.cloud.provider.resource;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by shenhongxi on 2017/9/14.
+ */
+@RestController
+public class DemoResource {
+    @Value("${server.port}")
+    private String port;
+
+    @RequestMapping("/hello")
+    public String hello(String name) {
+        return "Hi, " + name + ", Here is " + port;
+    }
+}
