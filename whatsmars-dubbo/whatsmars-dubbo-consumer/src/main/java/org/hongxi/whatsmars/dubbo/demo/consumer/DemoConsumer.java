923376c0ac1b040a6c20d7d16822c1cc5976cafa
javahongxi <javahongxi@qq.com>
2018-01-09 23:27:57 +0800

    补充新版相关依赖

--- whatsmars-dubbo/whatsmars-dubbo-consumer/src/main/java/org/hongxi/whatsmars/dubbo/demo/consumer/DemoConsumer.java
+++ whatsmars-dubbo/whatsmars-dubbo-consumer/src/main/java/org/hongxi/whatsmars/dubbo/demo/consumer/DemoConsumer.java
@@ -16,6 +16,9 @@ public class DemoConsumer {
 	
 	public static void main(String[] args) throws Exception {
 
+		//Prevent to get IPV6 address,this way only work in debug mode
+		//But you can pass use -Djava.net.preferIPv4Stack=true,then it work well whether in debug mode or not
+		System.setProperty("java.net.preferIPv4Stack", "true");
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[]{"META-INF/spring/dubbo-demo-consumer.xml"});
 		context.start();
 
--- whatsmars-dubbo/whatsmars-dubbo-provider/src/main/java/org/hongxi/whatsmars/dubbo/demo/provider/DemoProvider.java
+++ whatsmars-dubbo/whatsmars-dubbo-provider/src/main/java/org/hongxi/whatsmars/dubbo/demo/provider/DemoProvider.java
@@ -9,6 +9,9 @@ public class DemoProvider {
 
 	public static void main(String[] args) throws IOException {
 
+		//Prevent to get IPV6 address,this way only work in debug mode
+		//But you can pass use -Djava.net.preferIPv4Stack=true,then it work well whether in debug mode or not
+		System.setProperty("java.net.preferIPv4Stack", "true");
 		com.alibaba.dubbo.container.Main.main(args);
 
 	}
