ad1036053bff2e835d717b5b545189459423513c
javahongxi <javahongxi@qq.com>
2018-01-26 17:15:39 +0800

    lambda

--- whatsmars-javase-example/src/main/java/org/hongxi/whatsmars/base/lambda/stream/Test.java
+++ whatsmars-javase-example/src/main/java/org/hongxi/whatsmars/base/lambda/stream/Test.java
@@ -6,6 +6,10 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.partitioningBy;
+import static java.util.stream.Collectors.toList;
+
 /**
  * Created by shenhongxi on 2018/1/11.
  */
@@ -30,17 +34,19 @@ public class Test {
                 .reduce(0, Integer::sum);
         System.out.println(totalPoints);
 
-        Map<Status, List<Task>> map = tasks.stream().collect(Collectors.groupingBy(Task::getStatus));
+        Map<Status, List<Task>> map = tasks.stream().collect(groupingBy(Task::getStatus));
         System.out.println(map);
-        Map<Status, Long> map2 = tasks.stream().collect(Collectors.groupingBy(Task::getStatus, Collectors.counting()));
+        Map<Status, Long> map2 = tasks.stream().collect(groupingBy(Task::getStatus, Collectors.counting()));
         System.out.println(map2);
+        Map<Boolean, List<Task>> map3 = tasks.stream().collect(partitioningBy(e -> e.getPoint() > 7));
+        System.out.println(map3);
 
         Collection<String> result = tasks
                 .stream()
                 .mapToInt(Task::getPoint)
                 .mapToDouble(point -> point * 100 / totalPoints)
                 .mapToObj(percentage -> (int) percentage + "%")
-                .collect(Collectors.toList());
+                .collect(toList());
         System.out.println(result);
     }
 }
