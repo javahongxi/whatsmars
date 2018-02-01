67be3184562a6f73d90bcaaa0fa989c9e1820eaf
javahongxi <javahongxi@qq.com>
2018-02-01 23:02:40 +0800

    delete mongodb

--- whatsmars-spring/src/test/java/org/hongxi/whatsmars/spring/SpringTest.java
+++ whatsmars-spring/src/test/java/org/hongxi/whatsmars/spring/SpringTest.java
@@ -12,7 +12,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("/spring-context.xml")
-public class XxDemo {
+public class SpringTest {
     @Autowired
     private Mars mars;
 
