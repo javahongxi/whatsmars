3486bc611916568f01d302d6fdb1099abb2f7ca9
javahongxi <javahongxi@qq.com>
2018-02-02 16:31:48 +0800

    spring

--- whatsmars-spring/src/main/java/org/hongxi/whatsmars/spring/initializing/InitBean.java
+++ whatsmars-spring/src/main/java/org/hongxi/whatsmars/spring/initializing/InitBean.java
@@ -1,5 +1,6 @@
 package org.hongxi.whatsmars.spring.initializing;
 
+import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.stereotype.Component;
 
@@ -10,19 +11,24 @@ import javax.annotation.PreDestroy;
  * Created by shenhongxi on 2017/11/16.
  */
 @Component
-public class InitBean implements InitializingBean {
-    @Override
-    public void afterPropertiesSet() throws Exception {
-        System.out.println("================afterPropertiesSet");
-    }
-
+public class InitBean implements InitializingBean, DisposableBean {
     @PostConstruct
     public void init() {
         System.out.println("================init"); // before afterPropertiesSet
     }
 
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("================afterPropertiesSet");
+    }
+
     @PreDestroy
-    public void destroy() {
+    public void clear() {
+        System.out.println("================clear"); // before destroy
+    }
+
+    @Override
+    public void destroy() throws Exception {
         System.out.println("================destroy");
     }
 }
