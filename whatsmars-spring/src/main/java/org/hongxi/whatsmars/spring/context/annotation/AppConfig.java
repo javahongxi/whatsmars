de84c395a14808df3564a8f04ad5fe6e11d90be8
javahongxi <javahongxi@qq.com>
2018-02-06 10:58:09 +0800

    ComponentScan

--- whatsmars-spring/src/main/java/org/hongxi/whatsmars/spring/context/annotation/AppConfig.java
+++ whatsmars-spring/src/main/java/org/hongxi/whatsmars/spring/context/annotation/AppConfig.java
@@ -1,17 +1,24 @@
 package org.hongxi.whatsmars.spring.context.annotation;
 
+import org.hongxi.whatsmars.spring.context.annotation.service.DemoService;
 import org.hongxi.whatsmars.spring.model.Mars;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 /**
  * Created by javahongxi on 2017/10/31.
  */
 @Configuration
+@ComponentScan //("org.hongxi.whatsmars.spring.context.annotation.service")
 public class AppConfig {
+    @Autowired
+    private DemoService demoService;
 
     @Bean
     public Mars mars() {
+        demoService.service();
         return new Mars(1000, "火星");
     }
 }
---
+++ whatsmars-spring/src/main/java/org/hongxi/whatsmars/spring/context/annotation/service/DemoService.java
@@ -0,0 +1,14 @@
+package org.hongxi.whatsmars.spring.context.annotation.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by shenhongxi on 2018/2/6.
+ */
+@Service
+public class DemoService {
+
+    public void service() {
+        System.out.println("service...");
+    }
+}
