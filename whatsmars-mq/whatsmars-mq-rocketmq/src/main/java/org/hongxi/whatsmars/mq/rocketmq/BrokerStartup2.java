fc7dceecf673f8c423822d91a96329f165e25617
javahongxi <javahongxi@qq.com>
2018-09-11 18:46:30 +0800

    rename

--- whatsmars-mq/whatsmars-mq-rocketmq/src/main/java/org/hongxi/whatsmars/mq/rocketmq/BrokerStartup2.java
+++ whatsmars-mq/whatsmars-mq-rocketmq/src/main/java/org/hongxi/whatsmars/mq/rocketmq/BrokerStartup2.java
@@ -3,10 +3,10 @@ package org.hongxi.whatsmars.mq.rocketmq;
 /**
  * Created by shenhongxi on 2017/6/21.
  */
-public class Broker2Startup {
+public class BrokerStartup2 {
 
     public static void main(String[] args) {
-        String classpath = Broker2Startup.class.getResource("/").getPath();
+        String classpath = BrokerStartup2.class.getResource("/").getPath();
         args = new String[] {"-c", classpath + "broker2.properties"};
         org.apache.rocketmq.broker.BrokerStartup.main(args);
         System.out.println("Broker started. name: " + org.apache.rocketmq.broker.BrokerStartup.properties.getProperty("brokerName"));
