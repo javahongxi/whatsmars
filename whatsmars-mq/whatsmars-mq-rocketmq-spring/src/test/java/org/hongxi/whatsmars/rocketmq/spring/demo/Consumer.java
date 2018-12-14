4c332f5e78f3cb58a81cc617d1fff1f2acd3a604
javahongxi <javahongxi@qq.com>
2018-12-14 13:18:26 +0800

    move rocketmq spring demo

--- whatsmars-mq/whatsmars-mq-rocketmq-spring/src/test/java/org/hongxi/whatsmars/rocketmq/spring/demo/Consumer.java
+++ whatsmars-mq/whatsmars-mq-rocketmq-spring/src/test/java/org/hongxi/whatsmars/rocketmq/spring/demo/Consumer.java
@@ -4,7 +4,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Consumer {
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("spring/rocketmq-consumer.xml");
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("rocketmq-consumer.xml");
         context.registerShutdownHook();
     }
 }
--- whatsmars-mq/whatsmars-mq-rocketmq-spring/src/test/java/org/hongxi/whatsmars/rocketmq/spring/demo/Producer.java
+++ whatsmars-mq/whatsmars-mq-rocketmq-spring/src/test/java/org/hongxi/whatsmars/rocketmq/spring/demo/Producer.java
@@ -7,7 +7,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Producer {
 
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("spring/rocketmq-producer.xml");
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("rocketmq-producer.xml");
         RocketMQTemplate rocketMQTemplate = (RocketMQTemplate) context.getBean("rocketMQTemplate");
         for (int i = 0; i < 20; i++) {
             try {
