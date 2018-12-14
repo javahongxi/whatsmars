d2f82a98275fb8269605d88ddc34eae474889743
javahongxi <javahongxi@qq.com>
2018-12-14 18:38:23 +0800

    优化

--- whatsmars-mq/whatsmars-mq-rocketmq-tools/src/main/java/org/hongxi/whatsmars/rocketmq/MQAdminStartup.java
+++ whatsmars-mq/whatsmars-mq-rocketmq-tools/src/main/java/org/hongxi/whatsmars/rocketmq/MQAdminStartup.java
@@ -1,19 +1,17 @@
 package org.hongxi.whatsmars.rocketmq;
 
 import org.apache.rocketmq.common.MixAll;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
+
+import java.util.ResourceBundle;
 
 /**
  * Created by shenhongxi on 2018/10/21.
  */
-@SpringBootApplication
 public class MQAdminStartup {
 
     public static void main(String[] args) {
-        ConfigurableApplicationContext ctx = SpringApplication.run(MQAdminStartup.class, args);
-        System.setProperty(MixAll.ROCKETMQ_HOME_PROPERTY, ctx.getEnvironment().getProperty("rocketmqHome"));
+        ResourceBundle resourceBundle = ResourceBundle.getBundle("rocketmq");
+        System.setProperty(MixAll.ROCKETMQ_HOME_PROPERTY, resourceBundle.getString("rocketmqHome"));
         org.apache.rocketmq.tools.command.MQAdminStartup.main(args);
         args = new String[] {"help", "brokerStatus"};
         org.apache.rocketmq.tools.command.MQAdminStartup.main(args);
