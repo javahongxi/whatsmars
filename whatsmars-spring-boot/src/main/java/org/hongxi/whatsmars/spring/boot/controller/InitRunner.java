25804ba727f87d5c692b8b829ceb84bc11853f55
javahongxi <javahongxi@qq.com>
2019-01-11 18:33:51 +0800

    upgrade version to Rocket.S1

--- whatsmars-spring-boot/src/main/java/org/hongxi/whatsmars/spring/boot/controller/InitRunner.java
+++ whatsmars-spring-boot/src/main/java/org/hongxi/whatsmars/spring/boot/controller/InitRunner.java
@@ -1,16 +1,12 @@
 package org.hongxi.whatsmars.spring.boot.controller;
 
-import org.hongxi.whatsmars.redis.client.service.RedisService;
 import org.hongxi.whatsmars.spring.boot.async.MessageService;
 import org.hongxi.whatsmars.spring.boot.dao.UserMapper;
-import org.hongxi.whatsmars.spring.boot.model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.core.annotation.Order;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
 
 /**
@@ -26,12 +22,6 @@ public class InitRunner implements CommandLineRunner {
     private UserMapper userMapper;
     @Autowired
     private MessageService messageService;
-    @Autowired
-    private RedisService redisService;
-    @Autowired
-    private RedisTemplate redisTemplate;
-    @Autowired
-    private StringRedisTemplate stringRedisTemplate;
 
     @Override
     public void run(String... args) throws Exception {
@@ -41,27 +31,5 @@ public class InitRunner implements CommandLineRunner {
         for (int i = 0; i < 10; i++) {
             messageService.send("message" + i);
         }
-
-        String key = "domain";
-        redisService.set(key, "hongxi.org", 60); // 60s
-        System.out.println(redisService.get(key));
-
-        User user = new User();
-        user.setUsername("javahongxi");
-        user.setNickname("红喜");
-        redisService.set("user", user);
-        System.out.println(redisService.get("user"));
-        System.out.println(redisService.get("user", User.class));
-        redisService.sadd("countries", "China", "America", "Japan");
-        System.out.println(redisTemplate.opsForSet().isMember("countries", "China")); // fasle
-        System.out.println(stringRedisTemplate.opsForSet().isMember("countries", "China")); // true
-
-        redisService.hset("menu", "A", "a");
-        System.out.println(redisService.hget("menu", "A"));
-
-        redisService.leftPush("message", "haha");
-        redisService.leftPush("message", user);
-        System.out.println(redisService.rightPop("message"));
-        System.out.println(redisService.rightPop("message"));
     }
 }
