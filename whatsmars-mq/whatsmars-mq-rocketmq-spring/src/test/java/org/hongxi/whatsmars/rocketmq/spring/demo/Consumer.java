c61469fd0abc42f926921be16c0e92366618e7a4
javahongxi <javahongxi@qq.com>
2019-01-22 20:08:34 +0800

    rocketmq-spring

--- whatsmars-mq/whatsmars-mq-rocketmq-spring/src/test/java/org/hongxi/whatsmars/rocketmq/spring/demo/Consumer.java
+++ whatsmars-mq/whatsmars-mq-rocketmq-spring/src/test/java/org/hongxi/whatsmars/rocketmq/spring/demo/Consumer.java
@@ -7,7 +7,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  */
 public class Consumer {
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("rocketmq-consumer.xml");
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("mq-consumer.xml");
         context.registerShutdownHook();
     }
 }
