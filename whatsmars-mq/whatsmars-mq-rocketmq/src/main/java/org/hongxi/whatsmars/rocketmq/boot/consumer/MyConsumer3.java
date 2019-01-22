2d3cde6283deab8db204e6f5ca8429a7209521ed
javahongxi <javahongxi@qq.com>
2019-01-22 09:34:31 +0800

    ConditionalOnProperty

--- whatsmars-mq/whatsmars-mq-rocketmq/src/main/java/org/hongxi/whatsmars/rocketmq/boot/consumer/MyConsumer3.java
+++ whatsmars-mq/whatsmars-mq-rocketmq/src/main/java/org/hongxi/whatsmars/rocketmq/boot/consumer/MyConsumer3.java
@@ -4,15 +4,16 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.rocketmq.spring.starter.annotation.RocketMQMessageListener;
 import org.apache.rocketmq.spring.starter.core.RocketMQListener;
 import org.hongxi.whatsmars.rocketmq.boot.OrderPaidEvent;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Service;
 
 /**
  * 指定连接某个MQ集群
  */
-
+@ConditionalOnProperty(prefix = "trade.mq", value = {"nameServer"})
 @Slf4j
 @Service
-@RocketMQMessageListener(nameServer = "127.0.0.1:9877", instanceName = "tradeCluster", topic = "test-topic-3", consumerGroup = "my-consumer_test-topic-3")
+@RocketMQMessageListener(nameServer = "${trade.mq.nameServer}", instanceName = "${trade.mq.nameServer}", topic = "test-topic-3", consumerGroup = "my-consumer_test-topic-3")
 public class MyConsumer3 implements RocketMQListener<String> {
     public void onMessage(String message) {
         log.info("received message: " + message);
