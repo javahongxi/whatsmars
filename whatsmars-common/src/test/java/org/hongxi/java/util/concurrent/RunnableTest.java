6df276c873be60ac234d802ff4a000aa392bbbf0
javahongxi <javahongxi@qq.com>
2019-08-09 09:58:27 +0800

    concurrent test

--- whatsmars-common/src/test/java/org/hongxi/java/util/concurrent/RunnableTest.java
+++ whatsmars-common/src/test/java/org/hongxi/java/util/concurrent/RunnableTest.java
@@ -3,6 +3,7 @@ package org.hongxi.java.util.concurrent;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import java.util.stream.IntStream;
 
 /**
  * Created on 2019/8/8.
@@ -12,7 +13,10 @@ import java.util.concurrent.Future;
 public class RunnableTest {
 
     public static void main(String[] args) throws Exception {
-        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        IntStream.range(0, 10).forEach(n -> {
+            executorService.execute(() -> System.out.println(n));
+        });
         executorService.submit(() -> System.out.println("hello"));
 
         Future<String> future = executorService.submit(() ->
