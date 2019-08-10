e1f178b788d1681a3ed1bdbc804a4a9376cf4b87
javahongxi <javahongxi@qq.com>
2019-08-11 00:11:07 +0800

    concurrent test

---
+++ whatsmars-common/src/test/java/org/hongxi/java/util/concurrent/InterruptTest.java
@@ -0,0 +1,23 @@
+package org.hongxi.java.util.concurrent;
+
+/**
+ * Created on 2019/8/10.
+ *
+ * @author shenhongxi
+ */
+public class InterruptTest {
+
+    public static void main(String[] args) {
+        Thread t = new Thread(() -> {
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(Thread.currentThread().isInterrupted()); // false
+        });
+        t.start();
+        t.interrupt(); // Just to set the interrupt flag
+        System.out.println(t.isInterrupted()); // true
+    }
+}
---
+++ whatsmars-common/src/test/java/org/hongxi/java/util/concurrent/InterruptTest2.java
@@ -0,0 +1,18 @@
+package org.hongxi.java.util.concurrent;
+
+/**
+ * Created on 2019/8/10.
+ *
+ * @author shenhongxi
+ */
+public class InterruptTest2 {
+
+    public static void main(String[] args) {
+        Thread t = new Thread(() -> {
+            System.out.println(Thread.interrupted()); // currentThread().isInterrupted(true);
+            System.out.println(Thread.interrupted());
+        });
+        t.start();
+        t.interrupt(); // Just to set the interrupt flag
+    }
+}
