89e8bc1c0605af572caee6b139ad7bbefb56e146
javahongxi <javahongxi@qq.com>
2019-08-13 22:53:15 +0800

    concurrent test

--- whatsmars-common/src/test/java/org/hongxi/java/util/concurrent/CompletableFutureTest.java
+++ whatsmars-common/src/test/java/org/hongxi/java/util/concurrent/CompletableFutureTest.java
@@ -11,6 +11,35 @@ public class CompletableFutureTest {
         CompletableFuture.supplyAsync(() -> 1)
                 .thenApply(i -> i + 1)
                 .thenApply(i -> i * i)
-                .whenComplete((r, e) -> System.out.println(r));
+                .whenComplete((r, e) -> System.out.println("supplyAsync completed: " + r));
+
+        CompletableFuture.runAsync(() -> System.out.println("runAsync"))
+                .thenRun(() -> System.out.println("thenRun"))
+                .whenCompleteAsync((r, e) -> System.out.println("whenCompleteAsync"));
+
+        CompletableFuture.supplyAsync(() -> "Completable")
+                .thenApply(s -> s + " ")
+                .thenApply(s -> s + "Future")
+                .thenApply(s -> s + " ")
+                .thenCombine(CompletableFuture.completedFuture("Test"),
+                        (s1, s2) -> s1 + s2)
+                .thenApply(String::toUpperCase)
+                .exceptionally(e -> "error: " + e.getMessage())
+                .thenAccept(System.out::println);
+
+        CompletableFuture.supplyAsync(() -> 1)
+                .thenApply(i -> i++)
+                .handle((r, e) -> {
+                    if (e != null)
+                        return "error";
+                    else {
+                        System.out.println("handle result: " + r);
+                        return r;
+                    }
+                });
+
+        CompletableFuture.supplyAsync(() -> "f1")
+                .thenAcceptBoth(CompletableFuture.completedFuture("f2"),
+                        (r1, r2) -> System.out.println(String.join(",", r1, r2)));
     }
 }
