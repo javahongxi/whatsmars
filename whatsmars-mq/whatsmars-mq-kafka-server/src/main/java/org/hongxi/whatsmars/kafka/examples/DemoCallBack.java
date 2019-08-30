e18d67bfa81b1b6a63a73dbe57693ce004c96bea
javahongxi <javahongxi@qq.com>
2019-08-30 17:07:46 +0800

    kafka examples

---
+++ whatsmars-mq/whatsmars-mq-kafka-server/src/main/java/org/hongxi/whatsmars/kafka/examples/DemoCallBack.java
@@ -0,0 +1,38 @@
+package org.hongxi.whatsmars.kafka.examples;
+
+import org.apache.kafka.clients.producer.Callback;
+import org.apache.kafka.clients.producer.RecordMetadata;
+
+public class DemoCallBack implements Callback {
+
+        private final long startTime;
+        private final int key;
+        private final String message;
+
+        public DemoCallBack(long startTime, int key, String message) {
+            this.startTime = startTime;
+            this.key = key;
+            this.message = message;
+        }
+
+        /**
+         * A callback method the user can implement to provide asynchronous handling of request completion. This method will
+         * be called when the record sent to the server has been acknowledged. When exception is not null in the callback,
+         * metadata will contain the special -1 value for all fields except for topicPartition, which will be valid.
+         *
+         * @param metadata  The metadata for the record that was sent (i.e. the partition and offset). Null if an error
+         *                  occurred.
+         * @param exception The exception thrown during processing of this record. Null if no error occurred.
+         */
+        public void onCompletion(RecordMetadata metadata, Exception exception) {
+            long elapsedTime = System.currentTimeMillis() - startTime;
+            if (metadata != null) {
+                System.out.println(
+                        "message(" + key + ", " + message + ") sent to partition(" + metadata.partition() +
+                                "), " +
+                                "offset(" + metadata.offset() + ") in " + elapsedTime + " ms");
+            } else {
+                exception.printStackTrace();
+            }
+        }
+    }
--- whatsmars-mq/whatsmars-mq-kafka-server/src/main/java/org/hongxi/whatsmars/kafka/examples/Producer.java
+++ whatsmars-mq/whatsmars-mq-kafka-server/src/main/java/org/hongxi/whatsmars/kafka/examples/Producer.java
@@ -1,19 +1,3 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements. See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package org.hongxi.whatsmars.kafka.examples;
 
 import org.apache.kafka.clients.producer.*;
@@ -59,40 +43,6 @@ public class Producer extends Thread {
                 }
             }
             ++messageNo;
-        }
-    }
-}
-
-class DemoCallBack implements Callback {
-
-    private final long startTime;
-    private final int key;
-    private final String message;
-
-    public DemoCallBack(long startTime, int key, String message) {
-        this.startTime = startTime;
-        this.key = key;
-        this.message = message;
-    }
-
-    /**
-     * A callback method the user can implement to provide asynchronous handling of request completion. This method will
-     * be called when the record sent to the server has been acknowledged. When exception is not null in the callback,
-     * metadata will contain the special -1 value for all fields except for topicPartition, which will be valid.
-     *
-     * @param metadata  The metadata for the record that was sent (i.e. the partition and offset). Null if an error
-     *                  occurred.
-     * @param exception The exception thrown during processing of this record. Null if no error occurred.
-     */
-    public void onCompletion(RecordMetadata metadata, Exception exception) {
-        long elapsedTime = System.currentTimeMillis() - startTime;
-        if (metadata != null) {
-            System.out.println(
-                "message(" + key + ", " + message + ") sent to partition(" + metadata.partition() +
-                    "), " +
-                    "offset(" + metadata.offset() + ") in " + elapsedTime + " ms");
-        } else {
-            exception.printStackTrace();
         }
     }
 }
