48a8f0dc504f5b483f2bbd5a4fc3db6a42b6f804
javahongxi <javahongxi@qq.com>
2020-07-17 02:05:13 +0800

    test yml map

---
+++ whatsmars-spring-boot-samples/whatsmars-boot-sample-beans/src/main/java/org/hongxi/whatsmars/boot/sample/beans/TestProperties.java
@@ -0,0 +1,16 @@
+package org.hongxi.whatsmars.boot.sample.beans;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.util.Map;
+
+/**
+ * Created by shenhongxi on 2020/7/17.
+ */
+@Data
+@ConfigurationProperties(prefix = "test")
+public class TestProperties {
+
+    private Map<String, String> map;
+}
--- whatsmars-spring-boot-samples/whatsmars-boot-sample-beans/src/main/java/org/hongxi/whatsmars/boot/sample/beans/autoconfigure/ApplicationConfiguration.java
+++ whatsmars-spring-boot-samples/whatsmars-boot-sample-beans/src/main/java/org/hongxi/whatsmars/boot/sample/beans/autoconfigure/ApplicationConfiguration.java
@@ -7,6 +7,7 @@ import org.hongxi.whatsmars.boot.sample.beans.register.PlanetBeanDefinitionRegis
 import org.hongxi.whatsmars.boot.sample.beans.register.PlanetBeanDefinitionRegistryPostProcessor2;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
@@ -20,12 +21,14 @@ import java.util.List;
  * Created by shenhongxi on 2020/6/22.
  */
 @Configuration
+@EnableConfigurationProperties(TestProperties.class)
 public class ApplicationConfiguration {
 
     @Order(1)
     @ConditionalOnMissingBean
     @Bean
-    public DemoBean demoBean() {
+    public DemoBean demoBean(TestProperties testProperties) {
+        System.out.println(testProperties.getMap());
         return new DemoBean();
     }
 
@@ -62,7 +65,7 @@ public class ApplicationConfiguration {
         return new PlanetBeanDefinitionRegistryPostProcessor2(parseNames(environment));
     }
 
-    public List<String> parseNames(Environment environment) {
+    private List<String> parseNames(Environment environment) {
         List<String> names = new ArrayList<>();
         String configsKey = "planet.names[%d]";
         int configIndex = 0;
@@ -72,4 +75,5 @@ public class ApplicationConfiguration {
         }
         return names;
     }
+
 }
