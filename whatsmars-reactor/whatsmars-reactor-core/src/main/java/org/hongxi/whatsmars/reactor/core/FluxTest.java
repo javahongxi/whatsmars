c66ff78b85362b77f0ea2d180751933a7d45b6ec
javahongxi <javahongxi@qq.com>
2020-10-05 22:13:06 +0800

    FluxTest

--- whatsmars-reactor/whatsmars-reactor-core/src/main/java/org/hongxi/whatsmars/reactor/core/FluxTest.java
+++ whatsmars-reactor/whatsmars-reactor-core/src/main/java/org/hongxi/whatsmars/reactor/core/FluxTest.java
@@ -12,11 +12,36 @@ import java.util.Optional;
 public class FluxTest {
 
     public static void main(String[] args) {
+        Flux.range(1, 5).subscribe(System.out::println);
+
+        System.out.println("---");
+
+        Flux.just("A", "B", "C")
+                .subscribe(
+                        data -> System.out.println("onNext: " + data),
+                        err -> { /* ignored */ }
+                );
+
+        System.out.println("---");
+
         Flux.just("A", "B", "C")
                 .subscribe(
                         data -> System.out.println("onNext: " + data),
                         err -> { /* ignored */ },
                         () -> System.out.println("onComplete")
+                );
+
+        System.out.println("---");
+
+        Flux.range(1, 100)
+                .subscribe(
+                        data -> System.out.println("onNext: " + data),
+                        err -> { /* ignored */ },
+                        () -> System.out.println("onComplete"),
+                        subscription -> {
+                            subscription.request(4);
+                            subscription.cancel();
+                        }
                 );
     }
 
