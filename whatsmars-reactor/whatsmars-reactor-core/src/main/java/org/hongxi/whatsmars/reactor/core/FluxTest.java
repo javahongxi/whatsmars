20ce593ce79d28c2d3a85177ed8c2c633c48edf4
javahongxi <javahongxi@qq.com>
2020-10-05 20:24:55 +0800

    FluxTest

--- whatsmars-reactor/whatsmars-reactor-core/src/main/java/org/hongxi/whatsmars/reactor/core/FluxTest.java
+++ whatsmars-reactor/whatsmars-reactor-core/src/main/java/org/hongxi/whatsmars/reactor/core/FluxTest.java
@@ -12,6 +12,15 @@ import java.util.Optional;
 public class FluxTest {
 
     public static void main(String[] args) {
+        Flux.just("A", "B", "C")
+                .subscribe(
+                        data -> System.out.println("onNext: " + data),
+                        err -> { /* ignored */ },
+                        () -> System.out.println("onComplete")
+                );
+    }
+
+    public void base() {
         Flux<String> stream1 = Flux.just("Hello", "world");
         Flux<Integer> stream2 = Flux.fromArray(new Integer[] {1, 2, 3});
         Flux<Integer> stream3 = Flux.fromIterable(Arrays.asList(9, 8, 7));
@@ -20,5 +29,32 @@ public class FluxTest {
         Mono<String> stream5 = Mono.just("One");
         Mono<String> stream6 = Mono.justOrEmpty(null);
         Mono<String> stream7 = Mono.justOrEmpty(Optional.empty());
+
+        Flux<String> empty = Flux.empty();
+        Flux<String> never = Flux.never();
+        Mono<String> error = Mono.error(new RuntimeException("Unknown id"));
+    }
+
+    public void mono() {
+        Mono<String> stream8 = Mono.fromCallable(this::httpRequest);
+    }
+
+    public Mono<User> requestUserData(String sessionId) {
+         return Mono.defer(() ->
+                 isValidSession(sessionId)
+                 ? Mono.fromCallable(() -> requestUser(sessionId))
+                 : Mono.error(new RuntimeException("Invalid user session")));
+    }
+
+    private String httpRequest() {
+        return "OK";
+    }
+
+    private boolean isValidSession(String sessionId) {
+        return true;
+    }
+
+    private User requestUser(String sessionId) {
+        return new User();
     }
 }
