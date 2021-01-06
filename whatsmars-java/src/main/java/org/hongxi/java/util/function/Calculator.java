3e81e0d0ace50d2c9abf657278d47df5791344f3
javahongxi <javahongxi@qq.com>
2021-01-07 00:07:40 +0800

    函数式编程

--- whatsmars-java/src/main/java/org/hongxi/java/util/function/Calculator.java
+++ whatsmars-java/src/main/java/org/hongxi/java/util/function/Calculator.java
@@ -9,6 +9,10 @@ public interface Calculator {
     int action(int... numbers);
 
     static Calculator adder() {
+        // lambda表达式和匿名类有区别
+        // lambda表达式想要替代匿名类是有条件的，即这个匿名类
+        // 实现的接口必须是函数式接口，即只能有一个抽象方法的接口，
+        // 如 java.lang.Runnable
         return numbers -> {
             int sum = 0;
             for (Integer number : numbers) {
