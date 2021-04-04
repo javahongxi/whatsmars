ca469090982619dc67c5ba707d1b0192d690489d
javahongxi <javahongxi@qq.com>
2021-04-05 07:36:26 +0800

    2021.4.0

--- whatsmars-archetypes/whatsmars-initializr/src/main/resources/generate.sh
+++ whatsmars-archetypes/whatsmars-initializr/src/main/resources/generate.sh
@@ -29,7 +29,7 @@ mvn archetype:generate                                  \
   -DarchetypeCatalog=internal                           \
   -DarchetypeGroupId=org.hongxi                         \
   -DarchetypeArtifactId=${type}-archetype               \
-  -DarchetypeVersion=Rocket.S8                          \
+  -DarchetypeVersion=2021.4.0                          \
   -DgroupId=${groupId}                                  \
   -DartifactId=${artifactId}                            \
   -Dversion=1.0.0-SNAPSHOT                              \
