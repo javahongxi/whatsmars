41ac9b516e2a03335985e9fdac450d01406ace42
javahongxi <javahongxi@qq.com>
2021-04-26 19:33:31 +0800

    ExceptionFilter

--- whatsmars-spring-boot-samples/whatsmars-boot-sample-webflux/src/main/java/org/hongxi/whatsmars/boot/sample/webflux/filter/ExceptionFilter.java
+++ whatsmars-spring-boot-samples/whatsmars-boot-sample-webflux/src/main/java/org/hongxi/whatsmars/boot/sample/webflux/filter/ExceptionFilter.java
@@ -9,26 +9,22 @@ import org.springframework.web.server.WebFilterChain;
 import reactor.core.publisher.Mono;
 
 /**
- * Created by shenhongxi on 2021/4/22.
+ * Created by shenhongxi on 2021/4/26.
  */
 @Slf4j
-@Order(-1)
+@Order(-2)
 @Component
-public class LogFilter implements WebFilter {
+public class ExceptionFilter implements WebFilter {
+
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
-        preHandle(exchange);
         return chain.filter(exchange)
                 .doFinally(signalType -> postHandle(exchange));
     }
 
-    private void preHandle(ServerWebExchange exchange) {
-        log.info("preHandle");
-        exchange.getAttributes().put("test", true);
-        throw new RuntimeException("test exception");
-    }
-
     private void postHandle(ServerWebExchange exchange) {
         log.info("postHandle");
+        Boolean test = exchange.getAttribute("test");
+        log.info("attribute `test`: {}", test);
     }
 }
