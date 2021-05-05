3db315f7f0c55ac2f44de1c43d6417261b0ffca8
javahongxi <javahongxi@qq.com>
2021-05-05 12:20:04 +0800

    README

--- whatsmars-spring-boot-samples/whatsmars-boot-sample-webflux/src/main/java/org/hongxi/whatsmars/boot/sample/webflux/DemoApplication.java
+++ whatsmars-spring-boot-samples/whatsmars-boot-sample-webflux/src/main/java/org/hongxi/whatsmars/boot/sample/webflux/DemoApplication.java
@@ -5,6 +5,9 @@ import reactor.core.publisher.Hooks;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * WebFlux 更多解决方案见 https://github.com/javahongxi/webflux-sample
+ */
 @SpringBootApplication
 public class DemoApplication {
 
