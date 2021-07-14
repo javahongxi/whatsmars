eac8a3b0a54c161926890af350f16e6a69c350ee
javahongxi <javahongxi@qq.com>
2021-07-14 12:33:19 +0800

    Example

--- whatsmars-reactor/whatsmars-reactor-core/src/main/java/org/hongxi/whatsmars/reactor/core/Example.java
+++ whatsmars-reactor/whatsmars-reactor-core/src/main/java/org/hongxi/whatsmars/reactor/core/Example.java
@@ -1,9 +1,18 @@
 package org.hongxi.whatsmars.reactor.core;
 
+import reactor.core.publisher.Mono;
+
+import java.util.concurrent.CompletableFuture;
+
 /**
  * Created by shenhongxi on 2020/10/6.
  *
  * * @see org.hongxi.whatsmars.reactor.core.FluxTests
  */
 public class Example {
+
+    public static void main(String[] args) {
+        Mono.fromFuture(CompletableFuture.supplyAsync(() -> "123"))
+                .subscribe(System.out::println);
+    }
 }
