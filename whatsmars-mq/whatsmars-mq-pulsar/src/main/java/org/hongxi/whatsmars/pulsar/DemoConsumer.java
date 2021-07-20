8b6920678899a1a991d6954f1762ef92e936d75f
javahongxi <javahongxi@qq.com>
2021-07-20 12:31:57 +0800

    pulsar demo

---
+++ whatsmars-mq/whatsmars-mq-pulsar/src/main/java/org/hongxi/whatsmars/pulsar/DemoConsumer.java
@@ -0,0 +1,33 @@
+package org.hongxi.whatsmars.pulsar;
+
+import org.apache.pulsar.client.api.Consumer;
+import org.apache.pulsar.client.api.MessageListener;
+import org.apache.pulsar.client.api.PulsarClient;
+import org.apache.pulsar.client.api.PulsarClientException;
+
+/**
+ * Created by shenhongxi on 2021/7/20.
+ */
+public class DemoConsumer {
+
+    public static void main(String[] args) throws PulsarClientException {
+        PulsarClient client = PulsarClient.builder()
+                .serviceUrl("pulsar://localhost:6650")
+                .build();
+
+        MessageListener myMessageListener = (consumer, msg) -> {
+            try {
+                System.out.println("Message received: " + new String(msg.getData()));
+                consumer.acknowledge(msg);
+            } catch (Exception e) {
+                consumer.negativeAcknowledge(msg);
+            }
+        };
+
+        Consumer consumer = client.newConsumer()
+                .topic("my-topic")
+                .subscriptionName("my-subscription")
+                .messageListener(myMessageListener)
+                .subscribe();
+    }
+}
---
+++ whatsmars-mq/whatsmars-mq-pulsar/src/main/java/org/hongxi/whatsmars/pulsar/DemoProducer.java
@@ -0,0 +1,23 @@
+package org.hongxi.whatsmars.pulsar;
+
+import org.apache.pulsar.client.api.Producer;
+import org.apache.pulsar.client.api.PulsarClient;
+import org.apache.pulsar.client.api.PulsarClientException;
+
+/**
+ * Created by shenhongxi on 2021/7/20.
+ */
+public class DemoProducer {
+
+    public static void main(String[] args) throws PulsarClientException {
+        PulsarClient client = PulsarClient.builder()
+                .serviceUrl("pulsar://localhost:6650")
+                .build();
+
+        Producer<byte[]> producer = client.newProducer()
+                .topic("my-topic")
+                .create();
+
+        producer.send("My message".getBytes());
+    }
+}
