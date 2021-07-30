ffb9cd874b42fe88d1c148baea86805d2b63e49f
javahongxi <javahongxi@qq.com>
2021-07-30 18:50:21 +0800

    test @Validated

--- whatsmars-spring-boot-samples/whatsmars-boot-sample-web/src/main/java/org/hongxi/whatsmars/boot/sample/web/controller/SampleController.java
+++ whatsmars-spring-boot-samples/whatsmars-boot-sample-web/src/main/java/org/hongxi/whatsmars/boot/sample/web/controller/SampleController.java
@@ -3,18 +3,22 @@ package org.hongxi.whatsmars.boot.sample.web.controller;
 import org.hongxi.whatsmars.boot.sample.web.model.User;
 import org.hongxi.whatsmars.common.result.Result;
 import org.hongxi.whatsmars.common.result.ResultHelper;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.constraints.Min;
 
 /**
  * Created by shenhongxi on 2020/8/16.
  */
+@Validated
 @RestController
 @RequestMapping("/sample")
 public class SampleController {
 
     @GetMapping({"/hello", "hi"})
     public Result<String> hello(@RequestParam String userId,
-                                @RequestParam Integer age) {
+                                @RequestParam @Min(value = 18, message = "age must greater then 18") Integer age) {
         String hello = userId + "," + age;
         return ResultHelper.newSuccessResult(hello);
     }
--- whatsmars-spring-boot-samples/whatsmars-boot-sample-web/src/main/java/org/hongxi/whatsmars/boot/sample/web/exception/DefaultExceptionHandler.java
+++ whatsmars-spring-boot-samples/whatsmars-boot-sample-web/src/main/java/org/hongxi/whatsmars/boot/sample/web/exception/DefaultExceptionHandler.java
@@ -5,12 +5,17 @@ import org.hongxi.whatsmars.common.result.Result;
 import org.hongxi.whatsmars.common.result.ResultHelper;
 import org.springframework.core.annotation.AnnotationUtils;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.validation.ConstraintViolationException;
 
 /**
  * Created by shenhongxi on 2020/8/16.
@@ -26,10 +31,36 @@ public class DefaultExceptionHandler {
         return ResultHelper.newErrorResult(e.getCode(), e.getMsg());
     }
 
+    /**
+     * 不需要处理的异常
+     */
     @ResponseStatus(HttpStatus.OK)
-    @ExceptionHandler(Exception.class)
+    @ExceptionHandler(value = {HttpRequestMethodNotSupportedException.class})
     @ResponseBody
-    public Result<Void> handleException(HttpServletRequest request, Exception e) throws Exception {
+    public Result<Void> justThrow(HttpServletRequest request, Exception e) throws Exception {
+        throw e;
+    }
+
+    /**
+     * 处理请求参数异常
+     */
+    @ResponseStatus(HttpStatus.OK)
+    @ExceptionHandler(value = {MethodArgumentNotValidException.class,
+            MissingServletRequestParameterException.class,
+            MethodArgumentTypeMismatchException.class,
+            ConstraintViolationException.class})
+    @ResponseBody
+    public Result<Void> handleParamException(HttpServletRequest request, Exception e) throws Exception {
+        if (e instanceof ConstraintViolationException) {
+            return ResultHelper.newErrorResult(403, e.getMessage());
+        }
+        return ResultHelper.newErrorResult(403, "参数错误");
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @ExceptionHandler(Throwable.class)
+    @ResponseBody
+    public Result<Void> handleException(HttpServletRequest request, Throwable e) throws Throwable {
         if (AnnotationUtils.findAnnotation(e.getClass(), ResponseStatus.class) != null) {
             throw e;
         }
