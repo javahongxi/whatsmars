06bb3589d87bce43caceb9fb7c0adc9027c2043b
javahongxi <javahongxi@qq.com>
2023-05-28 19:30:33 +0800

    自定义sharding算法

--- whatsmars-shardingsphere/src/main/java/org/hongxi/whatsmars/shardingsphere/rule/MyTablePreciseShardingAlgorithm.java
+++ whatsmars-shardingsphere/src/main/java/org/hongxi/whatsmars/shardingsphere/rule/MyTablePreciseShardingAlgorithm.java
@@ -1,7 +1,8 @@
 package org.hongxi.whatsmars.shardingsphere.rule;
 
-import io.shardingsphere.api.algorithm.sharding.PreciseShardingValue;
-import io.shardingsphere.api.algorithm.sharding.standard.PreciseShardingAlgorithm;
+import org.apache.shardingsphere.sharding.api.sharding.standard.PreciseShardingValue;
+import org.apache.shardingsphere.sharding.api.sharding.standard.RangeShardingValue;
+import org.apache.shardingsphere.sharding.api.sharding.standard.StandardShardingAlgorithm;
 
 import java.util.Collection;
 
@@ -10,12 +11,17 @@ import java.util.Collection;
  *
  * @author shenhongxi
  */
-public class MyTablePreciseShardingAlgorithm implements PreciseShardingAlgorithm<Long> {
+public class MyTablePreciseShardingAlgorithm implements StandardShardingAlgorithm<Long> {
     @Override
     public String doSharding(Collection<String> collection, PreciseShardingValue<Long> preciseShardingValue) {
         String logicTableName = preciseShardingValue.getLogicTableName();
         Long orderId = preciseShardingValue.getValue();
 
         return String.join("_", logicTableName, String.valueOf(orderId % 2));
+    }
+
+    @Override
+    public Collection<String> doSharding(Collection<String> collection, RangeShardingValue<Long> rangeShardingValue) {
+        return null;
     }
 }
